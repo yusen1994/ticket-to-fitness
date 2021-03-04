d2bde6e58b95a09b4c819280060d4f8ebc2bf690
Sandeep Giri <sndpflashtv@gmail.com>
2021-03-04 18:08:54 +1100

    MVC Framework

---
+++ app/views/pages/index.php
@@ -0,0 +1,4 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+  <h1><?php echo $data['title']; ?></h1>
+  <p>This is the MVC PHP framework. Ticket To Fitness will be built on top of MVC framework.</p>
+<?php require APPROOT . '/views/inc/footer.php'; ?>
