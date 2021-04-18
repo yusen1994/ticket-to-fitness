15d862a53d056f09081f58a4b896fdb2d9ec5e9d
Sandeep Giri <sndpflashtv@gmail.com>
2021-04-18 19:47:46 +1000

    Skeleton Page created for View, Controller and Models

---
+++ app/controllers/Accounts.php
@@ -0,0 +1,24 @@
+<?php
+
+
+class Accounts extends Controller{
+
+        public function __construct(){
+
+            $this->checkModel = $this->model('Accounts');
+
+        }
+
+    
+
+}
+
+
+
+
+
+
+
+
+
+?>
