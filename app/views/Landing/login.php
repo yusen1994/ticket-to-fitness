8983fea484720906d58687b4969982983fa1c8cc
Sandeep Giri <sndpflashtv@gmail.com>
2021-04-24 23:53:22 +1000

    CSS and JS external file broken Link Fixed

--- app/views/Landing/login.php
+++ app/views/Landing/login.php
@@ -1,20 +1,4 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
-<!--HTML for Registeration and Login goes here, for CSS visit public folder-->
-<!--If you would like to create different page for login and register create a folder called accounts instead and create two new
-files, one for login and registeration-->
-<!--Small Change -->
 
-<span style="font-size: 3em; color: Tomato;">
-  <i class="fas fa-camera"></i>
-</span>
 
-<span style="font-size: 48px; color: Dodgerblue;">
-  <i class="fas fa-camera"></i>
-</span>
-
-<span style="font-size: 3rem;">
-  <span style="color: Mediumslateblue;">
-  <i class="fas fa-camera"></i>
-  </span>
-</span>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
