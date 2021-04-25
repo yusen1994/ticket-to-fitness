cf0f4c9cd6edf0fab40de7981b33ae2ffd72ae1d
sndpgiri <70873455+kofhearts@users.noreply.github.com>
2021-04-25 16:17:15 +1000

    Reg functionality added to display error message

--- app/views/Landing/register.php
+++ app/views/Landing/register.php
@@ -18,38 +18,76 @@ files, one for login and registeration-->
   					<form>
   						 <table>
 							<tr>
-								<td><label for="user_name">User Name</label></td>
-								<td><input type="text" id="user_name" name="user_name" required title=""></td>
+								<td><label for="username">User Name</label></td>
+								<td><input type="text" id="username" name="username" value = "<?php if(!empty($data['username'])) {echo $data['username']; } ?> " required title=""></td>
+								<?php 
+									if(!empty($data['username_err'])){
+										echo "<span>"
+										echo $data['username_err'];
+										echo "</span>"
+									}
+								?>
 							</tr>
 							<tr>
-								<td><label for="first_name">Frist Name</label></td>
-								<td><input type="text" id="first_name" name="first_name" required title=""></td>
+								<td><label for="firstname">Frist Name</label></td>
+								<td><input type="text" id="firstname" name="firstname" value = "<?php if(!empty($data['firstname'])) {echo $data['firstname']; } ?> "  required title=""></td>
+								<?php 
+									if(!empty($data['firstname_err'])){
+										echo "<span>"
+										echo $data['firstname_err'];
+										echo "</span>"
+									}
+								?>
 							</tr>
 							<tr>
-								<td><label for="last_name">Last Name</label></td>
-								<td><input type="text" id="last_name" name="last_name" required title=""></td>
+								<td><label for="lastname">Last Name</label></td>
+								<td><input type="text" id="lastname" name="lastname" value = "<?php if(!empty($data['lastname'])) {echo $data['lastname']; } ?> "  required title=""></td>
+								<?php 
+									if(!empty($data['lastname_err'])){
+										echo "<span>"
+										echo $data['lastname_err'];
+										echo "</span>"
+									}
+								?>
 							</tr>
 							<tr>
 								<td><label for="email">Email</label></td>
-								<td><input type="email" id="email" name="email" required title=""></td>
+								<td><input type="email" id="email" name="email" value = "<?php if(!empty($data['email'])) {echo $data['email']; } ?> "  required title=""></td>
+								<?php 
+									if(!empty($data['email_err'])){
+										echo "<span>"
+										echo $data['email_err'];
+										echo "</span>"
+									}
+								?>	
 							</tr>
 							<tr>
 								<td><label for="password">Password</label></td>
-								<td><input type="password" id="password" name="password" required></td>
+								<td><input type="password" id="password" name="password" value = "<?php if(!empty($data['password'])) {echo $data['password']; } ?> "  required></td>
+								<?php 
+									if(!empty($data['password_err'])){
+										echo "<span>"
+										echo $data['password_err'];
+										echo "</span>"
+									}
+								?>
 							</tr>
 							<tr>
-								<td><label for="confirm_password">Confirm password</label></td>
-								<td><input type="confirm_password" id="confirm_password" name="password" required></td>
+								<td><label for="cpassword">Confirm password</label></td>
+								<td><input type="password" id="cpassword" name="cpassword" value = "<?php if(!empty($data['cpassword'])) {echo $data['cpassword']; } ?> "  required></td>
+								<?php 
+									if(!empty($data['cpassword_err'])){
+										echo "<span>"
+										echo $data['cpassword_err'];
+										echo "</span>"
+									}
+								?>	
 							</tr>
 						</table>
-						<p><input type="submit" value="Register"></p>
+						<p><input type="submit" name = "submit" value="Register"></p>
   					</form>
   				</div>
-
-
-
-
-  			</div>
+			</div>
   		</div>
 
 <?php require APPROOT . '/views/inc/footer.php'; ?>
