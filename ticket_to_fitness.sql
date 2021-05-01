de39feb56835b2af186955363dcfe17619985349
yusen1994 <zhangyusen19940521@gmail.com>
2021-05-01 13:24:09 +1000

    Merge branch 'master' of https://github.com/yusen1994/ticket-to-fitness

--- ticket_to_fitness.sql
+++ ticket_to_fitness.sql
@@ -34,6 +34,8 @@ CREATE TABLE `users` (
   `lastname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL
+  `user_activation_code` varchar(255) NOT NULL,
+  `user_email_status`enum('not verified','verified') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
