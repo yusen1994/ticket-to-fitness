ce6724867d0d349e8cb28ecfd557a208dc3803ce
yusen1994 <zhangyusen19940521@gmail.com>
2021-05-01 13:24:57 +1000

    sql update

--- ticket_to_fitness.sql
+++ ticket_to_fitness.sql
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `users` update test
 --
 
 CREATE TABLE `users` (
