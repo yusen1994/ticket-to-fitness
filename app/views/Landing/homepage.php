49de2e2bd863b769fba3808ebf85ec549970f116
Lucas Howlett <lucasshowlett@gmail.com>
2021-05-25 20:39:04 +1000

    New landing page and footer

--- app/views/Landing/homepage.php
+++ app/views/Landing/homepage.php
@@ -3,94 +3,162 @@
 <?php require APPROOT . '/views/inc/navbar.php'; ?>
 
 <head>
+  <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Poppins'>
   <link rel="stylesheet" href="<?php echo URLROOT; ?>/public/css/style.css">
 </head>
 
 <body>
-  <div class="container-fluid py-2 px-0" style="background-color:#F2F2F2;">
+  <div class="container-fluid" style="background-color:#F2F2F2;">
 
     <div class="row justify-content-center">
-      <div class="col-sm-12 col-lg-6 px-0">
+      <div class="landing-image col-sm-12 col-lg-6 px-0">
         <img class="img-responsive" src="<?php echo URLROOT; ?>/images/gym1.jpg" alt="">
-        <div class="img-overlay">
-          <a href="<?php echo URLROOT; ?>/accounts/login" class="landing-button btn btn-lg mx-1">Register/Login</a> 
+        <div class="text-overlay-1">
+          <h3 class="land-text-1">Join the fitness</h3>
+        </div>
+        <div class="text-overlay-2">
+          <h3 class="land-text-2">revolution</h3>
         </div>
       </div>
     </div>
 
-
-    <div class="container px-0">
-      <div class="content py-5">
-        <div class="row mx-auto">
-          <div class="col landing-col-item">
-            <img src="<?php echo URLROOT; ?>/images/cycle.svg">
-          </div>
-          <div class="col landing-col-item">
-            <img src="<?php echo URLROOT; ?>/images/swim.svg">
-          </div>
-          <div class="col landing-col-item">
-            <img src="<?php echo URLROOT; ?>/images/meditate.svg">
-          </div>
-        </div>
-        <div class="row justify-content-center py-5">
-          <h3 class="landing-text-one">One Membership</h3>
-          <a href="#">
-            <h3 class="landing-text-two">Multiple Gyms</h3>
+    <div class="container" style="padding-top: 20px;">
+      <div class="row justify-content-center">
+        <div class="col-sm-1 col-lg-6">
+          <a href="<?php echo URLROOT; ?>/Accounts/login">
+            <button type="button" class="land-login-btn float-left">Sign up now</button>
           </a>
         </div>
-        <div class="row mx-auto">
-          <div class="col landing-col-item">
-            <img src="<?php echo URLROOT; ?>/images/weightlift.svg">
-          </div>
-          <div class="col landing-col-item">
-            <img src="<?php echo URLROOT; ?>/images/fight.svg">
-          </div>
-          <div class="col landing-col-item">
-            <img src="<?php echo URLROOT; ?>/images/treadmill.svg">
-          </div>
+      </div>
+      <div class="row justify-content-center" style="padding-top: 20px;">
+        <div class="col-sm-1 col-lg-6">
+          <button href="#" class="land-learnmore-btn float-right">Learn more</button>
         </div>
       </div>
     </div>
 
-    <div class="container px-0">
-      <div class="content py-5" style="background-color:#0B518D">
-        <div class="row mx-auto">
-          <div class="col landing-col-item">
-            <img src="<?php echo URLROOT; ?>/images/gymlogo-placeholder.svg">
-          </div>
-          <div class="col landing-col-item">
-            <img src="<?php echo URLROOT; ?>/images/gymlogo-placeholder.svg">
-          </div>
-          <div class="col landing-col-item">
-            <img src="<?php echo URLROOT; ?>/images/gymlogo-placeholder.svg">
-          </div>
+    <div class="container" style="padding-top: 20px; height: 600px;">
+      <div class="row justify-content-center">
+        <div class="col-sm-1 col-lg-6 text-left">
+          <h3 class="land-text-3"><span class="land-text-4">Activities</span> near you</h3>
         </div>
-        <div class="row justify-content-center py-5">
-          <h3 class="landing-text-three">Access to Your</h3>
+      </div>
+      <div class="row justify-content-center" style="padding-top: 20px;">
+        <div class="col-sm-1 col-lg-6 text-center">
           <a href="#">
-            <h3 class="landing-text-two">Favourite Gyms</h3>
+            <button type="button" class="land-seeall-btn">See all<span class="btn-arrow-1"><svg id="arrow" xmlns="http://www.w3.org/2000/svg" width="12.536" height="10.751" viewBox="0 0 12.536 10.751">
+                  <g id="Group_87" data-name="Group 87" transform="translate(0 0)">
+                    <path id="Path_103" data-name="Path 103" d="M.753,36.337a.9.9,0,0,1,.155-.011H9.752l-.193-.09a1.794,1.794,0,0,1-.507-.359L6.573,33.4a.928.928,0,0,1-.13-1.188.9.9,0,0,1,1.345-.117l4.485,4.485a.9.9,0,0,1,0,1.268h0L7.788,42.331a.9.9,0,0,1-1.345-.09.928.928,0,0,1,.13-1.188l2.476-2.485a1.794,1.794,0,0,1,.448-.327l.269-.121H.958a.928.928,0,0,1-.946-.753A.9.9,0,0,1,.753,36.337Z" transform="translate(0 -31.842)" />
+                  </g>
+                </svg>
+              </span></button>
           </a>
         </div>
-        <div class="row mx-auto">
-          <div class="col landing-col-item">
-            <img src="<?php echo URLROOT; ?>/images/gymlogo-placeholder.svg">
-          </div>
-          <div class="col landing-col-item">
-            <img src="<?php echo URLROOT; ?>/images/gymlogo-placeholder.svg">
-          </div>
-          <div class="col landing-col-item">
-            <img src="<?php echo URLROOT; ?>/images/gymlogo-placeholder.svg">
-          </div>
+      </div>
+    </div>
+
+    <div class="container-fluid" style="padding-top: 20px;">
+      <div class="row justify-content-center">
+        <div class="col-sm-1 col-lg-6 text-right">
+          <h3 class="land-text-5"><span class="land-text-4">Categories</span> for everyone</h3>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-sm-1 col-lg-6 category-col">
+          <a href="#" class="btn category-btn-left">
+            <span class="category-img-left"><img src="<?php echo URLROOT; ?>/images/weightlift.svg"></span>
+            Weightlifting
+            <span class="btn-arrow-left"><svg id="Group_108" data-name="Group 108" xmlns="http://www.w3.org/2000/svg" width="23.917" height="20.511" viewBox="0 0 23.917 20.511">
+                <path id="Path_103" data-name="Path 103" d="M1.437,40.418a1.709,1.709,0,0,1,.3-.022H18.606l-.368-.171a3.423,3.423,0,0,1-.967-.684L12.54,34.809a1.771,1.771,0,0,1-.248-2.267,1.711,1.711,0,0,1,2.567-.222l8.556,8.556a1.711,1.711,0,0,1,0,2.42h0l-8.556,8.556a1.711,1.711,0,0,1-2.567-.171,1.771,1.771,0,0,1,.248-2.267l4.723-4.74a3.423,3.423,0,0,1,.856-.625l.513-.231H1.827A1.771,1.771,0,0,1,.022,42.382,1.711,1.711,0,0,1,1.437,40.418Z" transform="translate(0 -31.842)" />
+              </svg>
+
+            </span>
+          </a>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-sm-1 col-lg-6 category-col">
+          <a href="#" class="btn category-btn-right float-right">
+            <span class="btn-arrow-right"><svg id="Group_108" data-name="Group 108" xmlns="http://www.w3.org/2000/svg" width="23.917" height="20.511" viewBox="0 0 23.917 20.511">
+                <path id="Path_103" data-name="Path 103" d="M1.437,40.418a1.709,1.709,0,0,1,.3-.022H18.606l-.368-.171a3.423,3.423,0,0,1-.967-.684L12.54,34.809a1.771,1.771,0,0,1-.248-2.267,1.711,1.711,0,0,1,2.567-.222l8.556,8.556a1.711,1.711,0,0,1,0,2.42h0l-8.556,8.556a1.711,1.711,0,0,1-2.567-.171,1.771,1.771,0,0,1,.248-2.267l4.723-4.74a3.423,3.423,0,0,1,.856-.625l.513-.231H1.827A1.771,1.771,0,0,1,.022,42.382,1.711,1.711,0,0,1,1.437,40.418Z" transform="translate(0 -31.842)" />
+              </svg>
+              Yoga
+            </span>
+            <span class="category-img-right"><img src="<?php echo URLROOT; ?>/images/meditate.svg"></span>
+          </a>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-sm-1 col-lg-6 category-col">
+          <a href="#" class="btn category-btn-left">
+            <span class="category-img-left"><img src="<?php echo URLROOT; ?>/images/treadmill.svg"></span>
+            Cardio
+            <span class="btn-arrow-left"><svg id="Group_108" data-name="Group 108" xmlns="http://www.w3.org/2000/svg" width="23.917" height="20.511" viewBox="0 0 23.917 20.511">
+                <path id="Path_103" data-name="Path 103" d="M1.437,40.418a1.709,1.709,0,0,1,.3-.022H18.606l-.368-.171a3.423,3.423,0,0,1-.967-.684L12.54,34.809a1.771,1.771,0,0,1-.248-2.267,1.711,1.711,0,0,1,2.567-.222l8.556,8.556a1.711,1.711,0,0,1,0,2.42h0l-8.556,8.556a1.711,1.711,0,0,1-2.567-.171,1.771,1.771,0,0,1,.248-2.267l4.723-4.74a3.423,3.423,0,0,1,.856-.625l.513-.231H1.827A1.771,1.771,0,0,1,.022,42.382,1.711,1.711,0,0,1,1.437,40.418Z" transform="translate(0 -31.842)" />
+              </svg>
+            </span>
+          </a>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-sm-1 col-lg-6 category-col">
+          <a href="#" class="btn category-btn-right float-right">
+            <span class="btn-arrow-right"><svg id="Group_108" data-name="Group 108" xmlns="http://www.w3.org/2000/svg" width="23.917" height="20.511" viewBox="0 0 23.917 20.511">
+                <path id="Path_103" data-name="Path 103" d="M1.437,40.418a1.709,1.709,0,0,1,.3-.022H18.606l-.368-.171a3.423,3.423,0,0,1-.967-.684L12.54,34.809a1.771,1.771,0,0,1-.248-2.267,1.711,1.711,0,0,1,2.567-.222l8.556,8.556a1.711,1.711,0,0,1,0,2.42h0l-8.556,8.556a1.711,1.711,0,0,1-2.567-.171,1.771,1.771,0,0,1,.248-2.267l4.723-4.74a3.423,3.423,0,0,1,.856-.625l.513-.231H1.827A1.771,1.771,0,0,1,.022,42.382,1.711,1.711,0,0,1,1.437,40.418Z" transform="translate(0 -31.842)" />
+              </svg>
+              Cycling
+              <span class="category-img-right"><img src="<?php echo URLROOT; ?>/images/cycle.svg"></span>
+            </span>
+          </a>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-sm-1 col-lg-6 category-col">
+          <a href="#" class="btn category-btn-left">
+            <span class="category-img-left"><img src="<?php echo URLROOT; ?>/images/swim.svg"></span>
+            Swimming
+            <span class="btn-arrow-left"><svg id="Group_108" data-name="Group 108" xmlns="http://www.w3.org/2000/svg" width="23.917" height="20.511" viewBox="0 0 23.917 20.511">
+                <path id="Path_103" data-name="Path 103" d="M1.437,40.418a1.709,1.709,0,0,1,.3-.022H18.606l-.368-.171a3.423,3.423,0,0,1-.967-.684L12.54,34.809a1.771,1.771,0,0,1-.248-2.267,1.711,1.711,0,0,1,2.567-.222l8.556,8.556a1.711,1.711,0,0,1,0,2.42h0l-8.556,8.556a1.711,1.711,0,0,1-2.567-.171,1.771,1.771,0,0,1,.248-2.267l4.723-4.74a3.423,3.423,0,0,1,.856-.625l.513-.231H1.827A1.771,1.771,0,0,1,.022,42.382,1.711,1.711,0,0,1,1.437,40.418Z" transform="translate(0 -31.842)" />
+              </svg>
+            </span>
+          </a>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-sm-1 col-lg-6 category-col">
+          <a href="#" class="btn category-btn-right float-right">
+            <span class="btn-arrow-right"><svg id="Group_108" data-name="Group 108" xmlns="http://www.w3.org/2000/svg" width="23.917" height="20.511" viewBox="0 0 23.917 20.511">
+                <path id="Path_103" data-name="Path 103" d="M1.437,40.418a1.709,1.709,0,0,1,.3-.022H18.606l-.368-.171a3.423,3.423,0,0,1-.967-.684L12.54,34.809a1.771,1.771,0,0,1-.248-2.267,1.711,1.711,0,0,1,2.567-.222l8.556,8.556a1.711,1.711,0,0,1,0,2.42h0l-8.556,8.556a1.711,1.711,0,0,1-2.567-.171,1.771,1.771,0,0,1,.248-2.267l4.723-4.74a3.423,3.423,0,0,1,.856-.625l.513-.231H1.827A1.771,1.771,0,0,1,.022,42.382,1.711,1.711,0,0,1,1.437,40.418Z" transform="translate(0 -31.842)" />
+              </svg>
+
+            </span>
+            Martial Arts
+            <span class="category-img-right"><img src="<?php echo URLROOT; ?>/images/fight.svg"></span>
+          </a>
+        </div>
+      </div>
+      <div class="row justify-content-center" style="padding-top: 20px;">
+        <div class="col-sm-1 col-lg-6 text-center">
+          <a href="#">
+            <button type="button" class="land-seeall-btn">See all<span class="btn-arrow-1"><svg id="arrow" xmlns="http://www.w3.org/2000/svg" width="12.536" height="10.751" viewBox="0 0 12.536 10.751">
+                  <g id="Group_87" data-name="Group 87" transform="translate(0 0)">
+                    <path id="Path_103" data-name="Path 103" d="M.753,36.337a.9.9,0,0,1,.155-.011H9.752l-.193-.09a1.794,1.794,0,0,1-.507-.359L6.573,33.4a.928.928,0,0,1-.13-1.188.9.9,0,0,1,1.345-.117l4.485,4.485a.9.9,0,0,1,0,1.268h0L7.788,42.331a.9.9,0,0,1-1.345-.09.928.928,0,0,1,.13-1.188l2.476-2.485a1.794,1.794,0,0,1,.448-.327l.269-.121H.958a.928.928,0,0,1-.946-.753A.9.9,0,0,1,.753,36.337Z" transform="translate(0 -31.842)" />
+                  </g>
+                </svg>
+              </span></button>
+          </a>
         </div>
       </div>
     </div>
 
-    <div class="container px-0">
-      <div class="content py-5">
-        <div class="row justify-content-center">
-          <div class="col">
-            <h3 style="text-align:center;">Testimonials</h3>
-          </div>
+    <div class="row justify-content-center" style="padding-top: 20px;">
+      <div class="landing-image col-sm-12 col-lg-6 px-0">
+        <img class="img-responsive" src="<?php echo URLROOT; ?>/images/gym2.jpg" alt="">
+        <div class="text-overlay-3">
+          <h3 class="land-text-1">Start</h3>
+        </div>
+        <div class="text-overlay-4">
+          <h3 class="land-text-2">Today</h3>
         </div>
       </div>
     </div>
@@ -99,10 +167,161 @@
 </body>
 
 
-
-
 <?php require APPROOT . '/views/inc/footer.php'; ?>
 
 <style>
+  .landing-image {
+    position: relative;
+    text-align: center;
+  }
 
+  .img-responsive {
+    width: 100%;
+  }
+
+  .text-overlay-1 {
+    position: absolute;
+    bottom: 35px;
+    right: 15px;
+
+  }
+
+  .text-overlay-2 {
+    position: absolute;
+    bottom: 0px;
+    right: 15px;
+  }
+
+  .text-overlay-3 {
+    position: absolute;
+    top: 10px;
+    left: 15px;
+
+  }
+
+  .text-overlay-4 {
+    position: absolute;
+    top: 10px;
+    left: 100px;
+  }
+
+  .land-text-1 {
+    font-size: 30px;
+    font-weight: bold;
+    color: white;
+    text-shadow: 0.5px 0 0 #000, 0 -0.5px 0 #000, 0 0.5px 0 #000, -0.5px 0 0 #000;
+  }
+
+  .land-text-2 {
+    font-size: 30px;
+    font-weight: bold;
+    color: #EF8830;
+    text-shadow: 0.5px 0 0 #000, 0 -0.5px 0 #000, 0 0.5px 0 #000, -0.5px 0 0 #000;
+  }
+
+  .land-text-3 {
+    font-size: 25px;
+    font-weight: bold;
+
+  }
+
+  .land-text-4 {
+    font-size: 25px;
+    font-weight: bold;
+    color: #EF8830;
+  }
+
+  .land-text-5 {
+    font-size: 25px;
+    font-weight: bold;
+    text-align: right;
+
+  }
+
+  .land-login-btn {
+    background-color: #EF8830;
+    font-family: 'Poppins';
+    font-weight: bold;
+    border-radius: 12px;
+    border: none;
+    width: 75%;
+    height: 45px;
+  }
+
+  .land-learnmore-btn {
+    background-color: white;
+    font-family: 'Poppins';
+    font-weight: bold;
+    border-radius: 12px;
+    border-color: #EF8830;
+    border-width: thin;
+    width: 75%;
+    height: 45px;
+  }
+
+  .land-seeall-btn {
+    background-color: white;
+    font-family: 'Poppins';
+    font-weight: bold;
+    border-radius: 12px;
+    border-color: #EF8830;
+    border-width: thin;
+    width: 75%;
+    height: 45px;
+  }
+
+  .btn-arrow-1 {
+    padding-left: 5px;
+
+  }
+
+  .category-btn-left {
+    width: 75%;
+    height: 75px;
+    align-items: left;
+    padding: 0px;
+    margin: 0px;
+    border-radius: 50px;
+    border-color: #3F3F3F;
+    border-width: 2px;
+    text-align: left;
+    position: relative;
+  }
+
+  .category-col {
+    padding: 0px;
+    margin: 0px;
+    padding-top: 20px;
+  }
+
+  .category-img-left {
+    margin-left: -1px;
+    margin-right: 5px;
+  }
+
+  .category-btn-right {
+    width: 75%;
+    height: 74px;
+    align-items: right;
+    padding: 0px;
+    margin: 0px;
+    border-radius: 50px;
+    border-color: #3F3F3F;
+    border-width: 2px;
+    text-align: right;
+  }
+
+  .category-img-right {
+    position: relative;
+    margin-right: -2px;
+  }
+
+  .btn-arrow-left {
+    position: absolute;
+    top: 35%;
+    right: 10%;
+
+  }
+
+  .btn-arrow-right {}
 </style>
--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,31 +1,72 @@
-
-    <div class="container" style="padding: 15px;">
-        <div class="row">
-            <div class="col">
-                <p>Fitch (C)</p>
-            </div>
-            <div class="col">
-                <a href="#">
-                    <p style="text-align: right;">About</p>
-                </a>
-            </div>
+<div class="container-fluid" style="background-color: #3F3F3F; padding-top: 20px;">
+    <div class="row justify-content-center">
+        <div class="col-sm-1 col-lg-6">
+            <a href="#" class="float-left">
+                <p style="color: white;">About</p>
+            </a>
+            <a href="#" class="float-right">
+                <p style="color: white;">Contact Us</p>
+            </a>
         </div>
-        <div class="row">
-            <div class="col">
+    </div>
+
+    <div class="row justify-content-center" style="padding-top: 20px;">
+        <div class="col-sm-1 col-lg-6 text-center">
+            <div class="social-container">
                 <a href="#">
-                    <p style="text-align: right;">Contact Us</p>
+                    <span class="insta-logo"><svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" viewBox="0 0 42 42">
+                            <g id="instagram" transform="translate(-0.449)">
+                                <path id="Path_108" data-name="Path 108" d="M42.342,12.348a15.344,15.344,0,0,0-.976-5.1,10.76,10.76,0,0,0-6.147-6.145,15.392,15.392,0,0,0-5.1-.976C27.874.025,27.16,0,21.457,0S15.039.025,12.8.123A15.357,15.357,0,0,0,7.7,1.1,10.25,10.25,0,0,0,3.977,3.528,10.34,10.34,0,0,0,1.556,7.245,15.385,15.385,0,0,0,.58,12.34C.473,14.588.449,15.3.449,21S.473,27.42.572,29.66a15.344,15.344,0,0,0,.977,5.1A10.758,10.758,0,0,0,7.695,40.9a15.394,15.394,0,0,0,5.1.976c2.24.1,2.954.123,8.658.123s6.417-.024,8.658-.123a15.348,15.348,0,0,0,5.1-.976,10.744,10.744,0,0,0,6.147-6.145,15.4,15.4,0,0,0,.976-5.1c.1-2.24.123-2.954.123-8.656s-.008-6.416-.107-8.656ZM38.559,29.5a11.51,11.51,0,0,1-.722,3.9,6.968,6.968,0,0,1-3.988,3.988,11.557,11.557,0,0,1-3.9.722c-2.216.1-2.88.123-8.485.123S15.187,38.2,12.98,38.1a11.51,11.51,0,0,1-3.9-.722,6.464,6.464,0,0,1-2.413-1.567A6.53,6.53,0,0,1,5.1,33.4a11.553,11.553,0,0,1-.722-3.9c-.1-2.215-.123-2.88-.123-8.484s.024-6.277.123-8.484A11.5,11.5,0,0,1,5.1,8.64,6.384,6.384,0,0,1,6.677,6.227,6.521,6.521,0,0,1,9.09,4.66a11.565,11.565,0,0,1,3.9-.722c2.216-.1,2.881-.123,8.485-.123s6.278.025,8.485.123a11.516,11.516,0,0,1,3.9.722,6.459,6.459,0,0,1,2.413,1.567A6.528,6.528,0,0,1,37.837,8.64a11.557,11.557,0,0,1,.722,3.9c.1,2.215.123,2.88.123,8.484s-.025,6.26-.123,8.476Zm0,0" transform="translate(0 0)" fill="#fff" />
+                                <path id="Path_109" data-name="Path 109" d="M137.08,124.5a12.127,12.127,0,1,0,12.131,12.127A12.132,12.132,0,0,0,137.08,124.5Zm0,19.994a7.867,7.867,0,1,1,7.869-7.867A7.869,7.869,0,0,1,137.08,144.494Zm0,0" transform="translate(-115.624 -115.623)" fill="#fff" />
+                                <path id="Path_110" data-name="Path 110" d="M366.828,90.791a2.189,2.189,0,1,1-2.189-2.189A2.189,2.189,0,0,1,366.828,90.791Zm0,0" transform="translate(-331.763 -81.202)" fill="#fff" />
+                            </g>
+                        </svg>
+                    </span>
+                </a>
+                <a href="#">
+                    <span class="facebook-logo"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="45" viewBox="0 0 22 45">
+                            <path id="facebook" d="M24.171,7.472h4.016V.317A50.721,50.721,0,0,0,22.335,0c-5.791,0-9.758,3.726-9.758,10.573v6.3H6.187v8h6.39V45h7.835V24.876h6.132l.973-8H20.411V11.366c0-2.312.61-3.894,3.76-3.894Z" transform="translate(-6.187)" fill="#fff" />
+                        </svg>
+                    </span>
+                </a>
+                <a href="#">
+                    <span class="twitter-logo"><svg xmlns="http://www.w3.org/2000/svg" width="42.855" height="37.243" viewBox="0 0 42.855 37.243">
+                            <g id="twitter" transform="translate(0 -48)">
+                                <g id="Group_106" data-name="Group 106" transform="translate(0 48)">
+                                    <path id="Path_107" data-name="Path 107" d="M42.855,52.409a17.4,17.4,0,0,1-5.062,1.484A9.26,9.26,0,0,0,41.658,48.7a16.882,16.882,0,0,1-5.571,2.275A8.548,8.548,0,0,0,29.669,48c-4.864,0-8.78,4.223-8.78,9.4a10.326,10.326,0,0,0,.2,2.143A24.374,24.374,0,0,1,2.984,49.713a9.969,9.969,0,0,0-1.2,4.75,9.6,9.6,0,0,0,3.9,7.81,8.248,8.248,0,0,1-3.969-1.157v.1a9.307,9.307,0,0,0,7.039,9.233,8.231,8.231,0,0,1-2.3.309,7.278,7.278,0,0,1-1.663-.16A8.969,8.969,0,0,0,13,77.147,16.937,16.937,0,0,1,2.1,81.155,15.4,15.4,0,0,1,0,81.026a23.58,23.58,0,0,0,13.478,4.217c16.167,0,25.006-14.324,25.006-26.741,0-.415-.013-.816-.032-1.215A18.25,18.25,0,0,0,42.855,52.409Z" transform="translate(0 -48)" fill="#fff" />
+                                </g>
+                            </g>
+                        </svg>
+                    </span>
                 </a>
             </div>
         </div>
     </div>
 
+    <div class="row justify-content-center" style="padding-top: 40px;">
+        <div class="col-sm-1 col-lg-6 text-center">
+            <p style="color: white; font-size: 12px;">Copyright (c) 2021 Fitch. All Rights Reserved.</p>
+        </div>
+    </div>
 
+</div>
+
+<style>
+    .insta-logo {
+        padding-right: 40px;
+    }
+
+    .twitter-logo {
+        padding-left: 40px;
+    }
+</style>
 
 <!--DOnot Remove This-->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<script src = "<?php echo URLROOT; ?>/js/main.js"></script>    
+<script src="<?php echo URLROOT; ?>/js/main.js"></script>
 </body>
+
 </html>
