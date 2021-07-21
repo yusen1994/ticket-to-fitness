64bdcc6d980630929a25c813213fdca928d304fb
Sandeep Giri <sndpflashtv@gmail.com>
2021-07-21 13:22:44 +1000

    Empty filter function

--- app/controllers/Activity.php
+++ app/controllers/Activity.php
@@ -5,7 +5,8 @@ class Activity extends Controller{
 
     public function __construct(){
 
-        
+        $this->activityModel = $this->model('ActivityModel');
+
 
     }
 
@@ -19,7 +20,20 @@ class Activity extends Controller{
         $this->view('Landing/activities');
     }
 
+    //Param from GET Request
+    //Param will be type which can be category, credit or frequency
+    //Return data, array of rows from database
+    //All the database query should be done in ActivityModel.php model
+    public function filterby($type, $param){
 
+        //Check if the type is category or credit or frequency
+        //if it is category then check what parameter it has like cycling, swimming etc based on that call ActivityModel function to get
+        //filtered result
+
+
+    }
+
+   
 
 }
 
---
+++ app/models/ActivityModel.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class GymModel
+{
+
+
+  private $db;
+
+  public function __construct()
+  {
+
+    $this->db = new Database;
+  }
+
+  
+}
+
