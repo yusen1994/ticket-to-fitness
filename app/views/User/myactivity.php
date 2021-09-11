76c3a7c4e2af637bdbdfbba04d661f8f8f3d2af0
Sandeep Giri <sndpflashtv@gmail.com>
2021-09-11 14:37:38 +1000

    removed horizontal scroll bar from myactivity

--- app/views/User/myactivity.php
+++ app/views/User/myactivity.php
@@ -1,7 +1,7 @@
 <?php require APPROOT . '/views/User/dashboardmenu.php'; ?>
 
 
-<div class="row justify-content-center" style="margin-top: 20px;">
+<div class="d-flex justify-content-center" style="margin-top: 20px;">
     <div class="col-sm-1 col-lg-4" style="margin-left: 15px;">
         <h3><b>Manage Activities</b></h3>
     </div>
@@ -61,7 +61,7 @@
                     $current = $single->activity_id;
                     echo '
                                     
-                <div class="row justify-content-center" style="margin-top:20px;  background-color: white;">
+                <div class="d-flex justify-content-center" style="margin-top:20px;  background-color: white;">
                 <div class="col-sm-1 col-lg-4" style="padding-right:0px;">
                 <div class="card-container">
                     <div class="row">
@@ -129,6 +129,7 @@
 
     </div>
 </div>
+    </div>
 
 
 <?php require APPROOT . '/views/inc/footer.php'; ?>
@@ -173,81 +174,3 @@
         font-size: 14px;
     }
 </style>
-<!--
-            <div class=" row justify-content-center">
-                        <div class="col">
-                            <?php
-                            if (!empty($data['myActivity'])) {
-                                foreach ($data['myActivity'] as $single) {
-
-                                    echo '<div class="container" style="padding-top: 20px; background-color: #f3f3f3">
-   
-  
-            <div class="card-container p-2" style="width:100%; height:auto;">
-                <div class="gym-image-container">
-                    <img class="gym-image" src="' . URLROOT . '/images/golds gym.png">
-                </div>
-                <div class="card-content"style="width:100%; height:auto;" >
-                    <p class="gym-name">' . $single->gym_name . '</p>
-                    <div class="line-separator"></div>
-                    <div class="gym-info-container">
-                        <table >
-                            <tr>
-                                <td class="card-info-col">
-                                    <img class="card-category-image" src="' . URLROOT . '/images/weightlift.svg">
-                                    <small class="card-cateogry-text">' . $single->category . '</small>
-                                </td>
-                                <td>
-                                    <img class="card-category-image" src="' . URLROOT . '/images/location-orange.svg">
-                                    <small class="card-cateogry-text">' . $single->gym_address . '</small>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="card-info-col">
-                                    <img class="card-category-image" src="' . URLROOT . '/images/timer-orange.svg">
-                                    <small class="card-cateogry-text">';
-                                    foreach ((unserialize($single->sessions_per_week)) as $session_per_week) {
-                                        echo $session_per_week . "  ";
-                                    }
-                                    echo '</small>
-                                </td>
-                                <td>
-                                    <img class="card-category-image" src="' . URLROOT . '/images/group-orange.svg">
-                                    <small class="card-cateogry-text">Unlimited</small>
-                                </td>
-                            </tr>
-                            
-                        </table>
-
-                    </div>
-                           
-
-                </div>
-                <div class="credits-container">
-                
-                    <img class="credits-image" class="card-category-image" src="' . URLROOT . '/images/credits.svg">
-                    <h5>25</h5>
-                    <a style="text-decoration:none" href = "' . URLROOT . '/User/removeActivity/' . $single->activity_id . '">
-                    
-                    <button type="button" class="btn pl-4"  style="color: red;"><b>Delete</b></button></a>
-
-                   
-                </div>
-            </div>
-            </div><br>';
-                                }
-                            } else {
-
-
-                                echo '<div class="alert alert-danger" role="alert">
-        ' . $data['error'] . '
-    </div>';
-                            }
-                            ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        -->
