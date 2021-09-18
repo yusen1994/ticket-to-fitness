017872c93fe3e9893161881ba84c82234fb02381
LHowlett97 <lucas.howlett1@gmail.com>
2021-09-18 17:01:56 +1000

    gym timetable UI

--- app/views/Gym/dashboardmenu.php
+++ app/views/Gym/dashboardmenu.php
@@ -106,8 +106,9 @@
                 $url = $_SERVER['REQUEST_URI'];
                 $urlarray = explode("/", $url);
                 $end = $urlarray[count($urlarray) - 1];
+                $secondLast = $urlarray[count($urlarray) - 2];
 
-                if (strpos($end, 'Timetable') !== false) {
+                if (strpos($end, 'Timetable') !== false || strpos($secondLast, 'timetable') !== false) {
                     echo
                     '
                     <div class=" col-4 text-center gymdashboard-menu-active" style="padding:0px !important; height:auto;">
